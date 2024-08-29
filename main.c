441cdc9dfaee6ce212c98b57a3c85c905460ff88
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-08-30 00:31:37 +0300

    Adding Print Function for Array Elements

--- main.c
+++ main.c
@@ -6,10 +6,13 @@ typedef unsigned int uint_32;
 
 void Execute_Bubble_Sort (uint_32 Data[], uint_32 Data_Length);
 void Swap_Two_Elements (uint_32 *num1, uint_32 *num2);
+void Print_Array_Elements (uint_32 Data[], uint_32 Data_Length);
 int main()
 {
     uint_32 Arr[ARRAY_MAX_SIZE] = {5, 3, 8, 4, 6};
+    Print_Array_Elements(Arr, ARRAY_MAX_SIZE);
     Execute_Bubble_Sort(Arr, ARRAY_MAX_SIZE);
+    Print_Array_Elements(Arr, ARRAY_MAX_SIZE);
     return 0;
 }
 
@@ -26,6 +29,7 @@ void Execute_Bubble_Sort (uint_32 Data[], uint_32 Data_Length)
             {
                 Swap_Two_Elements(&(Data[Adjacent_Iteration]), &(Data[Adjacent_Iteration+1]));
             }
+            else{/* Nothing */}
         }
         
     }
@@ -36,4 +40,14 @@ void Swap_Two_Elements (uint_32 *num1, uint_32 *num2)
     uint_32 Temp = *num2;
     *num2 = *num1;
     *num1 = Temp;
+}
+
+void Print_Array_Elements (uint_32 Data[], uint_32 Data_Length)
+{
+    uint_32 Iterator = 0;
+    for (Iterator = 0; Iterator < Data_Length; Iterator++)
+    {
+        printf("%i \t", Data[Iterator]);
+    }
+    printf("\n");
 }
